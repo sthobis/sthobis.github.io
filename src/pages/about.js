1c29868f3670e18b734c96410de12a0243559ac3
sthobis <s.thobi.sinaga@gmail.com>
2018-08-01 17:04:21 +0700

    feat: update about section and mobile views

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,13 @@ import "../styles/about.less";
 
 const About = () => (
   <div className="about">
-    <h1>ABOUT</h1>
-    <p>Hi, I'm a freelance web/mobile developer based on Bandar Lampung, Indonesia. You can call me Thobi.</p>
+    <h1>HI!</h1>
+    <p>I'm a freelance web/mobile developer with interest on UI design.</p>
+    <p>I am based on Bandar Lampung, Indonesia. You can call me Thobi.</p>
     <p>I spent most of my time developing web/mobile apps using Javascript, especially React for FrontEnd and Node/Express for BackEnd.</p>
-    <p>I started working as Front End Developer for Xtremax Pte. Ltd. on 2015 and resigned from the company on 2016 to start my career as a freelance dev.</p>
+    <p>I started working as Front End Developer for Xtremax Pte. Ltd. on 2015 and resigned from the company on 2016. I've been freelancing since then.</p>
+    <p>I spent my free time like an average Joe would (watching movies, playing games, etc).</p>
+    <p><br/>You can find me on twitter @iBoht and instagram @sthobis</p>
     <Nav/>
   </div>
 )
